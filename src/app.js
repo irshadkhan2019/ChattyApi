e2fe1719b6178a52d69ad4f90c37c74c1a5eea1e
Irshad Khan <khanirshad2019@gmail.com>
2022-12-05 11:35:25 +0530

    Handle Process Exceptions

--- src/app.js
+++ src/app.js
@@ -11,11 +11,51 @@ class Application {
     const app = express();
     const server = new ChattyServer(app);
     server.start();
+    Application.handleExit();
   }
+
   loadConfig() {
     console.log("loading configuration file");
     config.validateConfig();
     config.cloudinaryConfig();
+  }
+
+  static handleExit() {
+    process.on("uncaughtException", (error) => {
+      console.log(`There was an uncaught error: ${error}`);
+      Application.shutDownProperly(1);
+    });
+
+    process.on("unhandleRejection", (reason) => {
+      console.log(`Unhandled rejection at promise: ${reason}`);
+      Application.shutDownProperly(2);
+    });
+
+    process.on("SIGTERM", () => {
+      console.log("Caught SIGTERM");
+      Application.shutDownProperly(2);
+    });
+
+    process.on("SIGINT", () => {
+      console.log("Caught SIGINT");
+      Application.shutDownProperly(2);
+    });
+
+    process.on("exit", () => {
+      console.log("Exiting");
+    });
+  }
+
+  static shutDownProperly(exitCode) {
+    Promise.resolve()
+      .then(() => {
+        log.info("Shutdown complete");
+        process.exit(exitCode);
+      })
+      .catch((error) => {
+        log.error(`Error during shutdown: ${error}`);
+        process.exit(1);
+      });
   }
 }
 
