f8d8e077434cf28634d298f73f4f8471fcb8d7de
Irshad Khan <khanirshad2019@gmail.com>
2023-02-21 10:56:40 +0530

    Create groovy script

---
+++ script.groovy
@@ -0,0 +1,14 @@
+def buildApp() {
+  echo "Building the application..."
+}
+
+def testApp() {
+  echo "testing  the application..."
+}
+
+def deployApp() {
+  echo "deploying the application..."
+  echo "deploying version ${params.VERSION}"
+}
+
+return this
